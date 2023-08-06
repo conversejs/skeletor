1aa964773998b4d8a13840e89d481e5d5a36ea21
JC Brand <jc@opkode.com>
2023-08-06 20:38:10 +0200

    Remove the `View` type

--- karma.conf.js
+++ karma.conf.js
@@ -28,7 +28,6 @@ module.exports = function (config) {
       'test/noconflict.js',
       'test/router.js',
       'test/sync.js',
-      'test/view.js',
     ],
 
     // list of files to exclude
@@ -107,7 +106,7 @@ module.exports = function (config) {
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
-    singleRun: true,
+    singleRun: false,
 
     // Concurrency level
     // how many browser should be started simultaneous
